77df074930c7389efbc8e9e3c6170d1f253333af
Andrew <andredbearded@gmail.com>
2025-07-27 14:05:52 +0000

    Fix token env

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.getenv("7929661479:AAG4tT6oK6WXSWG9lPBmGo4KvGGtqLZ7sG4")
+TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Бот работает на Render 🚀")
