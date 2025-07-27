cb9edce469a15d67ec8a80bf2c4c3238554d9069
Andrew <andredbearded@gmail.com>
2025-07-27 14:27:53 +0000

    Update files with fixed bot code

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")  # берем токен из переменной окружения
+TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Бот работает на Render 🚀")
